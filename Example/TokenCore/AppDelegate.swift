43625277b93220dda440b0d3012c66a1522fa0de
tokyoliyi <tokyoliyi@139.com>
2019-08-23 15:07:55 +0800

    upgrade example

--- Example/TokenCore/AppDelegate.swift
+++ Example/TokenCore/AppDelegate.swift
@@ -1,13 +1,12 @@
 //
 //  AppDelegate.swift
-//  Example
+//  TokenCore
 //
-//  Created by James Chen on 2018/03/26.
-//  Copyright © 2018 ConsenLabs. All rights reserved.
+//  Created by Neal Xu on 11/01/2018.
+//  Copyright (c) 2018 Neal Xu. All rights reserved.
 //
 
 import UIKit
-import TokenCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,14 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+      
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        let rootVC = IdentityViewController()
+//        let navController = UINavigationController(rootViewController: rootVC)
+//        window?.rootViewController = navController
+//        window?.makeKeyAndVisible()
+//      
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -31,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
